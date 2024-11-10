b868b38cc7e990b8e82a8c41f3ad67a5b5ff773e
Veselin Martionv <veselin.martinov@gmail.com>
2024-11-10 16:08:15 +0100

    Created User Models

--- petstagram/accounts/models.py
+++ petstagram/accounts/models.py
@@ -1,3 +1,56 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 # Create your models here.
+
+
+class AppUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        null=False,
+        blank=False,
+        unique=True,
+    )
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+UserModel = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        UserModel,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    first_name = models.CharField(
+        max_length=30,
+        blank=True,
+        null=True,
+    )
+
+    last_name = models.CharField(
+        max_length=30,
+        blank=True,
+        null=True,
+    )
+
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    profile_picture = models.ImageField(
+        blank=True,
+        null=True,
+    )
