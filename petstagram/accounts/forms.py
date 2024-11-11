b678cc1987d8fc2e7901bc3e006ba0716844a1c4
Veselin Martionv <veselin.martinov@gmail.com>
2024-11-11 17:19:27 +0100

    Created forms for login and registration

---
+++ petstagram/accounts/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+UserModel = get_user_model()
+
+
+class AppUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = UserModel
+        fields = ('email',)
+
+
+class AppUserLoginForm(AuthenticationForm):
+    username = forms.EmailField(widget=forms.TextInput(attrs={'autofocus': True}))
+    password = forms.CharField(
+        label='Password',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'})
+    )
