6b083071a436897b1ea1dbb59a63726b1fd46f8a
Veselin Martionv <veselin.martinov@gmail.com>
2024-11-11 17:20:32 +0100

    created proper register and login views and added them to urls

--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -1,14 +1,37 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.views import LoginView, LogoutView
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from petstagram.accounts.forms import AppUserCreationForm, AppUserLoginForm
+from petstagram.accounts.models import Profile
 
 # Create your views here.
 
-
-def register(request):
-    return render(request, template_name='accounts/register-page.html')
+UserModel = get_user_model()
 
 
-def login(request):
-    return render(request, template_name='accounts/login-page.html')
+class AppUserRegisterView(CreateView):
+    model = UserModel
+    form_class = AppUserCreationForm
+    template_name = 'accounts/register-page.html'
+    success_url = reverse_lazy('login')
+
+
+class AppUserLoginView(LoginView):
+    form_class = AppUserLoginForm
+    template_name = 'accounts/login-page.html'
+
+    def form_valid(self, form):
+        super().form_valid(form)
+        profile_instance = Profile.objects.get_or_create(user=self.request.user)
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class AppUserLogoutView(LogoutView):
+    pass
 
 
 def show_profile_details(request, pk: int):
